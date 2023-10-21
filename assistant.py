5471ea548a4b94a2c95301ac263b84cac5f08346
Andanchenko Maria <andanchenko.maria@gmail.com>
2023-10-22 02:17:19 +0300

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -8,20 +8,58 @@ class Field:
         return str(self.value)
 
 class Name(Field):
-    pass
+    def __str__(self):
+        return str(self.value)
 
 class Phone(Field):
-    pass
+    def __str__(self):
+        return str(self.value)
 
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
 
-    pass
-
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+    
+    def add_phone(self, phone):
+        self.phones.append(phone)
 
 class AddressBook(UserDict):
-    pass
+    def add_record(self, record):
+        pass
+
+if __name__ == "__main__":
+     # Створення нової адресної книги
+    book = AddressBook()
+
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
