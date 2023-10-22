e9ee4edeb62a0b79557e3002dbc416117b065d9e
Andanchenko Maria <andanchenko.maria@gmail.com>
2023-10-22 20:19:45 +0300

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -1,5 +1,6 @@
 from collections import UserDict
 
+
 class Field:
     def __init__(self, value):
         self.value = value
@@ -7,14 +8,16 @@ class Field:
     def __str__(self):
         return str(self.value)
 
+
 class Name(Field):
     def __str__(self):
         return str(self.value)
 
+
 class Phone(Field):
     def __str__(self):
         return str(self.value)
-    
+
     def __init__(self, value):
         if self.validate_phone(value):
             super().__init__(value)
@@ -24,6 +27,7 @@ class Phone(Field):
     def validate_phone(self, phone):
         return len(phone) == 10 and phone.isdigit()
 
+
 class Record:
     def __init__(self, name):
         self.name = Name(name)
@@ -31,7 +35,7 @@ class Record:
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-        
+
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
 
@@ -48,10 +52,8 @@ class Record:
         for phone in self.phones:
             if phone.value == serched_phone:
                 return phone
-        return 'Phone not found'
+        return "Phone not found"
 
-    
-    
 
 class AddressBook(UserDict):
     def add_record(self, record):
@@ -60,15 +62,16 @@ class AddressBook(UserDict):
     def delete(self, name):
         if name in self.data:
             del self.data[name]
-    
+
     def find(self, name):
         if name in self.data:
             return self.data[name]
         else:
             return None
 
+
 if __name__ == "__main__":
-     # Створення нової адресної книги
+    # Створення нової адресної книги
     book = AddressBook()
 
     # Створення запису для John
